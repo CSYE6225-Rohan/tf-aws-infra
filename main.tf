dd31fd1f73d5edcbcbf4122f10d2dbfb67ac1068
rohanjauhari1312 <jauhari.r@northeastern.edu>
2025-02-23 23:42:22 -0500

    retreiving latest ami

--- main.tf
+++ main.tf
@@ -122,11 +122,23 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-resource "aws_instance" "app_instance" {
-  ami                    = var.custom_ami
+data "aws_ami" "latest_ubuntu_ami" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["custom-ubuntu-24.04-ami*"]
+  
+  }
+}
+
+resource "aws_instance" "webapp_instance" {
+  ami                    = data.aws_ami.latest_ubuntu_ami.id
   instance_type          = "t2.micro"  
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   subnet_id              = aws_subnet.public[0].id
+  associate_public_ip_address = true
   key_name               = var.key_name
 
   root_block_device {
@@ -147,4 +159,9 @@ resource "aws_instance" "app_instance" {
   tags = {
     Name = "WebApp-Instance"
   }
+
+  
+}
+output "public_ip" {
+  value = aws_instance.webapp_instance.public_ip
 }
