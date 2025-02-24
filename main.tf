094fa551d2931baaddf39d6fcc77f8a790013f5e
rohanjauhari1312 <jauhari.r@northeastern.edu>
2025-02-24 15:28:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count = length(var.azs)
 
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.main.bad
   cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = var.azs[count.index]
 
